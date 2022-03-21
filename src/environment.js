979a03fdbc84cb84615b667d37b799ea338f8f07
damian spizzirri <damianspizzirri@gmail.com>
2022-03-20 22:57:20 -0300

    crea clase Firebase para refactorizar codigo

---
+++ src/environment.js
@@ -0,0 +1,10 @@
+export const development = {
+    apiKey: "AIzaSyDaqdWIj43RwOB2o_kOP2CzBKZpbecHBbM",
+    authDomain: "clubphilidorajedrez.firebaseapp.com",
+    databaseURL: "https://clubphilidorajedrez.firebaseio.com",
+    projectId: "clubphilidorajedrez",
+    storageBucket: "clubphilidorajedrez.appspot.com",
+    messagingSenderId: "8430141422",
+    appId: "1:8430141422:web:e7d7b2c6c55c987b36d8ff",
+    measurementId: "G-RQE3CX5Q7Q"
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import './index.css';
-import _ from 'lodash';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+//import _ from 'lodash';
+import Firebase from './firebase';
+import { development } from './environment';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDaqdWIj43RwOB2o_kOP2CzBKZpbecHBbM",
-  authDomain: "clubphilidorajedrez.firebaseapp.com",
-  databaseURL: "https://clubphilidorajedrez.firebaseio.com",
-  projectId: "clubphilidorajedrez",
-  storageBucket: "clubphilidorajedrez.appspot.com",
-  messagingSenderId: "8430141422",
-  appId: "1:8430141422:web:e7d7b2c6c55c987b36d8ff",
-  measurementId: "G-RQE3CX5Q7Q"
-}
+const firebase = new Firebase(development);
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const news = firebase.getData('noticias');
+const school = firebase.getData('escuela');
 
-async function obtenerTorneos(db) {
-
-  const torneosCollection = collection(db, 'torneos');
-  const torneosSnapshot = await getDocs(torneosCollection);
-  const torneosList = torneosSnapshot.docs.map(doc => doc.data());
-  console.log(torneosList);
-}
-
-obtenerTorneos();
+console.log(news);
+console.log(school);
