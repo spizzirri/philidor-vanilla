5fe759dcb5090347586cf16f8657dd987173ce1d
damian spizzirri <damianspizzirri@gmail.com>
2022-06-27 21:20:46 -0300

    version beta

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './index.css';
-//import _ from 'lodash';
-import Firebase from './firebase';
-import { development } from './environment';
-
-const firebase = new Firebase(development);
-
-const news = firebase.getData('noticias');
-const school = firebase.getData('escuela');
-
-console.log(news);
-console.log(school);
